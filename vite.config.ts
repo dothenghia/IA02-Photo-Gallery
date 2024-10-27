21d72a5c10c9aa36a1263a1e7b89d3a2c7667c0a
The Nghia <nghiask2018@gmail.com>
2024-10-27 21:01:45 +0700

    [feat] add pwa to application

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,49 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Photo Gallery",
+        short_name: "Photo Gallery",
+        description: "PWA Photo Gallery of The Nghia",
+        icons: [
+          {
+            "src": "/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          }
+        ],
+        theme_color: '#FFFFFF',
+        background_color: '#FFFFFF',
+        display: "standalone",
+        start_url: "/",
+        orientation: 'portrait'
+      }
+    })],
 })
