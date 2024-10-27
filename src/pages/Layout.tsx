28375fb2952b70c5473f897f5e5b73cbf98a948e
The Nghia <nghiask2018@gmail.com>
2024-10-27 15:17:15 +0700

    [feat] handle load photos logic

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,9 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <div className='flex h-screen overflow-auto bg-slate-50'>
-      <Outlet />
-    </div>
+    <Outlet />
   )
 }
 
--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { getPhotos } from '../services';
 import { Link } from 'react-router-dom';
 
+// Just get necessary fields from the API response
 interface Photo {
   id: string;
   urls: {
@@ -16,41 +17,61 @@ const List: React.FC = () => {
   const [photos, setPhotos] = useState<Photo[]>([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const initialFetchDone = useRef(false);
 
-  const fetchPhotos = async () => {
+  // Fetch photos from the API
+  const fetchPhotos = useCallback(async () => {
+    if (!hasMore || loading) return; // Avoid multiple requests
+
     setLoading(true);
     try {
       const newPhotos = await getPhotos(page);
-      setPhotos((prevPhotos) => [...prevPhotos, ...newPhotos]);
-      setPage((prevPage) => prevPage + 1);
-    } catch (error) {
+      if (newPhotos.length === 0) {
+        setHasMore(false);
+      } else {
+        setPhotos(prevPhotos => [...prevPhotos, ...newPhotos]);
+        setPage(prevPage => prevPage + 1);
+      }
+    }
+    catch (error) {
       console.error('Error fetching photos:', error);
-    } finally {
+    }
+    finally {
       setLoading(false);
     }
-  };
+  }, [page, hasMore, loading]);
 
+  // Initial fetch
   useEffect(() => {
-    fetchPhotos();
-  }, []);
+    if (!initialFetchDone.current) {
+      fetchPhotos();
+      initialFetchDone.current = true;
+    }
+  }, [fetchPhotos]);
 
-  const handleScroll = () => {
+  // Infinite scroll
+  const handleScroll = useCallback(() => {
     if (
-      window.innerHeight + document.documentElement.scrollTop ===
-      document.documentElement.offsetHeight
+      window.innerHeight + document.documentElement.scrollTop >=
+      document.documentElement.offsetHeight - 100 &&
+      photos.length > 0 &&
+      !loading &&
+      hasMore
     ) {
       fetchPhotos();
     }
-  };
+  }, [fetchPhotos, photos.length, loading, hasMore]);
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+  }, [handleScroll]);
 
   return (
-    <div className="container mx-auto px-4">
-      <h1 className="text-3xl font-bold my-8">Unsplash Photos</h1>
+    <div className="flex-1 container mx-auto px-5 pb-10">
+      <h1 className="text-3xl font-bold my-8">Unsplash Photo Gallery</h1>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {photos.map((photo) => (
           <Link to={`/photos/${photo.id}`} key={photo.id} className="bg-white rounded-lg shadow-md overflow-hidden">
@@ -65,7 +86,9 @@ const List: React.FC = () => {
           </Link>
         ))}
       </div>
-      {loading && <p className="text-center my-4">Loading...</p>}
+
+      {loading && <div className="loader mx-auto my-6"></div>}
+      {!hasMore && <p className="text-center my-6">No more photos to load</p>}
     </div>
   );
 };
